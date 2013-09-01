1ed809c3d536c73f4584ceb283d9167d87bc2ed2
Aaron Richiger <a.richi@bluewin.ch>
2013-09-01 18:23:16 +0200

    Added slides.

--- examples/ex1/main.py
+++ examples/ex1/main.py
@@ -12,7 +12,13 @@ if __name__ == '__main__':
 
     # Create a Label and show it
     label = QLabel("Hello World")
+    label.resize(200, 100)
     label.show()
 
     # Enter Qt application main loop
     sys.exit(app.exec_())
+
+
+
+
+
